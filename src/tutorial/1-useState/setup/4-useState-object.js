b0cbf0bf37b065c570af217b6303c7be10eeddea
Doc-Jebus <saunders_wyndham@hotmail.com>
2022-09-27 02:38:07 +0900

    useState section complete.

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: "Peter",
+    age: 24,
+    message: "random message",
+  });
+
+  //You can setup as many useStates values as you wish... but it might get messy.
+  const [name, setName] = useState("Peter");
+  const [age, setAge] = useState(24);
+  const [message, setMessage] = useState("random message");
+
+  //Using the spread operator leave the other state values and only affecting the message element.
+  function changeMessage() {
+    //setPerson({ ...person, message: "Hello World!" });
+    setMessage("Hello World!");
+    setName("James");
+    setAge(43);
+  }
+  return (
+    <>
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h3>{message}</h3>
+      <button className='btn' onClick={changeMessage}>
+        change message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
