f205986ff36f5eccda15d17c56061ca190dc62a4
Doc-Jebus <saunders_wyndham@hotmail.com>
2022-09-28 00:38:14 +0900

    Complete toggle using operators

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,21 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
   //As the state value is an empty string, it is falsy.
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
+  const [isError, setIsError] = useState(false);
 
-  //value of text or if falsy, hello world.
-  const firstValue = text || 'hello world';
-  //If it is not true, return the value. Otherwise return hello world.
-  const secondValue = text && 'hello world';
-  
+  //The 'or' operator (||) value says that if the state is falsy, then return the alternative ('hello world').
+  //const firstValue = text || "hello world";
+  //The 'and' operator says the if the state is truthy, return the value ('hello world').
+  //const secondValue = text && "hello world";
 
-  return <>
-  <h1>{firstValue}</h1>
-  <h1>value: {secondValue}</h1>
-  </>
+  return (
+    <>
+      {/*<h1>{firstValue}</h1>
+      <h1>value: {secondValue}</h1>*/}
+
+      <h1>{text || "james"}</h1>
+      {/* Created a toggle button, switching between true and false */}
+      <button className='btn' onClick={() => setIsError(!isError)}>
+        toggle error
+      </button>
+      {isError && <h1>Error...</h1>}
+
+      {isError ? (
+        <p>there is an error...</p>
+      ) : (
+        <div>
+          <h2>there is no error.</h2>
+        </div>
+      )}
+      {/*<h1>value: {text && "paul"}</h1>
+      {/* This flips it by saying 'if it is not true...' 
+      {!text && <h1>hello world</h1>}*/}
+    </>
+  );
 };
 
 export default ShortCircuit;
--- src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,38 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
+  const [show, setShow] = useState(false);
+  return (
+    <>
+      <button className='btn' onClick={() => setShow(!show)}>
+        show/hide
+      </button>
+      {show && <Item />}
+    </>
+  );
+};
+
+const Item = () => {
+  const [size, setSize] = useState(window.innerWidth);
+
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+
+  //IMPORTANT NOTE: Use a cleanup function to prevent unnecessary allocations. After addEventListener, initiate removeEventListener.
+  useEffect(() => {
+    window.addEventListener("resize", checkSize);
+    return () => {
+      window.removeEventListener("resize", checkSize);
+    };
+  }, []);
+
+  return (
+    <div style={{ marginTop: "2rem" }}>
+      <h1>window</h1>
+      <h2>size : {size}px</h2>
+    </div>
+  );
 };
 
 export default ShowHide;
